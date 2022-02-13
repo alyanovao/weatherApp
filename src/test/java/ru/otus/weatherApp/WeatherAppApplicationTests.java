8d48eb853690df4e126e7b983eb67a48fa1405c5
Альянов Артем Олегович <domovoy175@gmail.com>
2022-02-14 00:03:48 +0300

    first commit

---
+++ src/test/java/ru/otus/weatherApp/WeatherAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.weatherApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
